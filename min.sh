97a272979ddaf639c4f09c3962157b575a8e6fe7
dongxin <dongxin2019@gmail.com>
2022-06-20 14:51:55 +0800

    update

---
+++ min.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dir1='js'
+dir2='styles'
+outdir='./lib/'
+function compression () {
+    dirlist=`ls $1`
+    echo $dirlist
+    for file in $dirlist
+    do
+        if [ ${file##*.} = "js" ]; then
+            uglifyjs $1/$file -m -o lib/js/${file%.*}.min.${file##*.}
+        fi
+        if [ ${file##*.} = "css" ]; then
+            cleancss -o lib/styles/${file%.*}.min.${file##*.} $1/$file
+        fi
+    done
+}
+
+compression $dir1
+compression $dir2
